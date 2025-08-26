b3656b52471431b0d35e4d50e238c51463d364aa
heeyon <dev.hyjang@gmail.com>
2025-08-26 10:45:01 +0900

    데이터 수집 스케줄러 적용

--- src/main/java/com/ccp/simple/controller/NewsApiController.java
+++ src/main/java/com/ccp/simple/controller/NewsApiController.java
@@ -2,24 +2,12 @@ package com.ccp.simple.controller;
 
 import com.ccp.simple.domain.News;
 import com.ccp.simple.service.NewsService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -28,76 +16,8 @@ public class NewsApiController {
 
     private final NewsService newsService;
 
-    @GetMapping("/newsCollect")
-    public void collectNews() throws JsonProcessingException {
-        String clientId = "DO2YnEI8qqODmwZOYP8N"; //애플리케이션 클라이언트 아이디
-        String clientSecret = "6ilVZfSoiE"; //애플리케이션 클라이언트 시크릿
-
-        String searchKeyword = "날씨";   // 검색어
-        if (searchKeyword == null || searchKeyword.isEmpty()) {
-            throw new RuntimeException("검색어가 비어 있습니다.");
-        }
-        String encodeQuery = URLEncoder.encode(searchKeyword, StandardCharsets.UTF_8);
-
-
-        String apiURL = "https://openapi.naver.com/v1/search/news?query=" + encodeQuery;
-
-        Map<String, String> requestHeaders = new HashMap<>();
-        requestHeaders.put("X-Naver-Client-Id", clientId);
-        requestHeaders.put("X-Naver-Client-Secret", clientSecret);
-
-        String responseBody = get(apiURL,requestHeaders);
-
-        newsService.insertNews(responseBody);
-    }
-
     @GetMapping("/news")
     public List<News> getAllNews() {
         return newsService.getAllNews();
-    }
-
-    private static String get(String apiUrl, Map<String, String> requestHeaders){
-        HttpURLConnection con = connect(apiUrl);
-        try {
-            con.setRequestMethod("GET");
-            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
-                con.setRequestProperty(header.getKey(), header.getValue());
-            }
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
-                return readBody(con.getInputStream());
-            } else { // 오류 발생
-                return readBody(con.getErrorStream());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("API 요청과 응답 실패", e);
-        } finally {
-            con.disconnect();
-        }
-    }
-
-    private static HttpURLConnection connect(String apiUrl){
-        try {
-            URL url = new URL(apiUrl);
-            return (HttpURLConnection)url.openConnection();
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
-        } catch (IOException e) {
-            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
-        }
-    }
-
-    private static String readBody(InputStream body){
-        InputStreamReader streamReader = new InputStreamReader(body);
-        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
-            StringBuilder responseBody = new StringBuilder();
-            String line;
-            while ((line = lineReader.readLine()) != null) {
-                responseBody.append(line);
-            }
-            return responseBody.toString();
-        } catch (IOException e) {
-            throw new RuntimeException("API 응답을 읽는 데 실패했습니다.", e);
-        }
     }
 }
